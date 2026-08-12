e02ceebc3e9cab68f0938e63447d525a6b352a02
a <x@gmail.com>
2026-08-12 16:42:29 -0500

    Modificacion del index

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+name = "Miguel Marin"
+contacto = "elba.zurita@gmail.com"
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html', person=name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
